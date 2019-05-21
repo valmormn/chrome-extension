e8ff01ed31f9f46447ee1b0d36b2afcc9169a821
valmor <valmormn@gmail.com>
2019-05-21 16:20:21 -0300

    chrome-extension

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 // content.js
 
-alert("Hello from your Chrome extension!");
+// alert("Hello from your Chrome extension!");
+
+window.alert("Ok! E agora?");
+
+console.log('esse veio da extensao');
+
+// document.body.style.backgroundColor = "red";
+
 
 // 
