9dad713c9ea32d93e25567df47f5013a49e411bd
Valmor <valmormn@gmail.com>
2019-05-22 18:29:02 -0300

    chrome-extension

---
+++ z.sh
@@ -0,0 +1,94 @@
+% IANA WHOIS server
+% for more information on IANA, visit http://www.iana.org
+% This query returned 1 object
+
+refer:        whois.registro.br
+
+domain:       BR
+
+organisation: Comite Gestor da Internet no Brasil
+address:      Av. das Nações Unidas, 11541, 7° andar
+address:      São Paulo  SP 04578-000
+address:      Brazil
+
+contact:      administrative
+name:         Demi Getschko
+organisation: Comite Gestor da Internet no Brasil
+address:      Av. das Nações Unidas, 11541, 7° andar
+address:      São Paulo  SP 04578-000
+address:      Brazil
+phone:        +55 11 5509 3505
+fax-no:       +55 11 5509 3501
+e-mail:       demi@registro.br
+
+contact:      technical
+name:         Frederico Augusto de Carvalho Neves
+organisation: Registro .br
+address:      Av. das Nações Unidas, 11541, 7° andar
+address:      São Paulo  SP 04578-000
+address:      Brazil
+phone:        +55 11 5509 3505
+fax-no:       +55 11 5509 3501
+e-mail:       fneves@registro.br
+
+nserver:      A.DNS.BR 200.160.0.10 2001:12ff:0:0:0:0:0:10
+nserver:      B.DNS.BR 200.189.41.10
+nserver:      C.DNS.BR 200.192.233.10
+nserver:      D.DNS.BR 200.219.154.10 2001:12f8:4:0:0:0:0:10
+nserver:      E.DNS.BR 200.229.248.10 2001:12f8:1:0:0:0:0:10
+nserver:      F.DNS.BR 200.219.159.10 2001:12f8:c:0:0:0:0:10
+ds-rdata:     2471 13 2 5E4F35998B8F909557FA119C4CBFDCA2D660A26F069EF006B403758A07D1A2E4
+
+whois:        whois.registro.br
+
+status:       ACTIVE
+remarks:      Registration information: http://registro.br/
+
+created:      1989-04-18
+changed:      2018-08-21
+source:       IANA
+
+
+% Copyright (c) Nic.br
+%  The use of the data below is only permitted as described in
+%  full by the terms of use at https://registro.br/termo/en.html ,
+%  being prohibited its distribution, commercialization or
+%  reproduction, in particular, to use it for advertising or
+%  any similar purpose.
+%  2019-05-22T15:39:07-03:00
+
+domain:      valmor.net.br
+owner:       VALMOR MARTINS DO NASCIMENTO
+ownerid:     053.580.717-17
+country:     BR
+owner-c:     VANAS65
+admin-c:     VANAS65
+tech-c:      VANAS65
+billing-c:   VANAS65
+nserver:     iris.ns.cloudflare.com
+nsstat:      20190522 AA
+nslastaa:    20190522
+nserver:     ray.ns.cloudflare.com
+nsstat:      20190522 AA
+nslastaa:    20190522
+saci:        yes
+created:     20160821 #16007418
+changed:     20190516
+expires:     20190821
+status:      published
+provider:    ENDURANCE-BRASIL (43)
+
+nic-hdl-br:  VANAS65
+person:      VALMOR NASCIMENTO
+e-mail:      valmormn@gmail.com
+country:     BR
+created:     20150531
+changed:     20180312
+
+% Security and mail abuse issues should also be addressed to
+% cert.br, http://www.cert.br/ , respectivelly to cert@cert.br
+% and mail-abuse@cert.br
+%
+% whois.registro.br accepts only direct match queries. Types
+% of queries are: domain (.br), registrant (tax ID), ticket,
+% provider, contact handle (ID), CIDR block, IP and ASN.
